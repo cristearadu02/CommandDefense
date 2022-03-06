795e7dafa851b21025d84a932004c8ff157acf67
cristearadu02 <andrei.cristea@info.uaic.ro>
2022-03-06 21:26:40 +0200

    Week 2 CD

--- week2/ex2/GlobalWave.cpp
+++ week2/ex2/GlobalWave.cpp
@@ -1,20 +1,19 @@
-// ConsoleApplication10.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "GlobalWave.h"
 
-#include <iostream>
-
-int main()
+int compareEnemiesNumber(Wave* wave1, Wave* wave2)
 {
-    std::cout << "Hello World!\n";
+    if (wave1->get_nrofEnemies() == wave2->get_nrofEnemies())
+        return 0;
+    if (wave1->get_nrofEnemies() > wave2->get_nrofEnemies())
+        return 1;
+    return -1;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int compareMoney(Wave* wave1, Wave* wave2)
+{
+    if (wave1->get_moneyperEnemy() == wave2->get_moneyperEnemy())
+        return 0;
+    if (wave1->get_moneyperEnemy() > wave2->get_moneyperEnemy())
+        return 1;
+    return -1;
+}
